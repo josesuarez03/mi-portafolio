0153da1b1110d0c2cb0a95e75c4be2f7c0f4c7a7
José Suarez <102729396+josesuarez03@users.noreply.github.com>
2025-05-30 23:35:00 +0200

    añadiendo configuracion para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  output: 'static',
   integrations: [react()],
 
 });
